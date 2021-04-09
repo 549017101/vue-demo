6210307af3801fe9a4132f61ef315b9168d12ffc
pxz <549017101@qq.com>
2021-04-09 15:34:28 +0800

    配置了postcss-px-to-viewport插件用来转化长度单位

---
+++ postcss.config.js
@@ -0,0 +1,20 @@
+/**
+ * Author: pxz
+ * Email: buffll@foxmail.com
+ * 2021-04-09 15:00
+ */
+module.exports = {
+  plugins: {
+    autoprefixer: {},
+    "postcss-px-to-viewport": {
+      viewportWidth: 375,             // 视窗的宽度，对应的是我们设计稿的宽度
+      viewportHeight: 667,            // 视窗的高度 （也可以不配置）
+      unitPrecision: 5,               // 指定'px'转化为视窗单位值的小数位数（很多时候无法整除）
+      viewportUnit: 'vw',             // 指定需要转换成的视窗单位，建议使用vw
+      selectorBlackList: ['ignore'],  // 指定不需要转化的类
+      minPixelValue: 1,               // 小于或等于`1px`不转化未视窗单位
+      mediaQuery: false,              // 允许在媒体查询中转换`px`
+      exclude: [/TabBar/]            // 指定不需要转化的文件
+    }
+  }
+}
